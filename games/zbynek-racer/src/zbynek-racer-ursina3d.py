47ae6c4cd7ee55d6522c12183b6f93697b174dc6
Zbynek Vyskovsky <kvr000@gmail.com>
2026-03-16 17:33:50 -0700

    Race car object design

--- games/zbynek-racer/src/zbynek-racer-ursina3d.py
+++ games/zbynek-racer/src/zbynek-racer-ursina3d.py
@@ -1,55 +1,72 @@
 #!/usr/bin/env python3
+import math
 
-from ursina import *
+from ursina import Ursina, DirectionalLight, AmbientLight, Entity, Cylinder, color, copy, camera, time
 
 app = Ursina()
 DirectionalLight()
 AmbientLight(color=color.rgba(100,100,100,0.5))
 
-def make_car():
-    car = Entity()
-    # Body
-    Entity(parent=car, model='cube', color=color.azure, scale=(1, 0.3, 3))
-    # Nose
-    Entity(parent=car, model='cube', color=color.azure, scale=(0.4, 0.2, 1.2), position=(0, 0, 2))
-    # Rear wing
-    Entity(parent=car, model='cube', color=color.black, scale=(1.2, 0.1, 0.3), position=(0, 0.35, -1.4))
-    # Front wing
-    Entity(parent=car, model='cube', color=color.black, scale=(1.4, 0.1, 0.3), position=(0, 0.15, 3))
+class RaceCar(Entity):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.make_car()
+        self.rotation_y += 45
 
-    wheel_width = 0.4
-    wheel_mesh = Cylinder(radius=0.3, height=wheel_width, resolution=32)
-    def make_wheel(x, z):
-        #Entity(parent=car, model='cube', color=color.black, scale=(0.4, 0.4, 0.4), position=(x, 0, z))
-        return Entity(
-            parent=car,
-            model=copy(wheel_mesh),
-            color=color.black,
-            position=(x - wheel_width/2, 0, z),
-            rotation=(0,0,90),   # axle left-right
-        )
+    def make_car(self):
+        # Body
+        Entity(parent=self, model='cube', color=color.azure, scale=(1, 0.3, 3))
+        # Nose
+        Entity(parent=self, model='cube', color=color.azure, scale=(0.4, 0.2, 1.2), position=(0, 0, 2))
+        # Rear wing
+        Entity(parent=self, model='cube', color=color.black, scale=(1.2, 0.1, 0.3), position=(0, 0.35, -1.4))
+        # Front wing
+        Entity(parent=self, model='cube', color=color.black, scale=(1.4, 0.1, 0.3), position=(0, 0.15, 3))
 
-    # Wheels
-    car.wheels = []
-    car.wheels.append(make_wheel(-0.9, 1.5))
-    car.wheels.append(make_wheel(0.9, 1.5))
-    car.wheels.append(make_wheel(-0.9, -1.5))
-    car.wheels.append(make_wheel( 0.9, -1.5))
+        wheel_width = 0.4
+        wheel_position = 0.7
+        wheel_mesh = Cylinder(radius=0.3, height=wheel_width, resolution=32)
+        axle_mesh = Cylinder(radius=0.05, height=wheel_position, resolution=32)
+        def make_wheel(x, z):
+            #Entity(parent=car, model='cube', color=color.black, scale=(0.4, 0.4, 0.4), position=(x, 0, z))
+            wheel = Entity(
+                parent=self,
+                model=copy(wheel_mesh),
+                color=color.black,
+                position=(x, 0, z),
+                rotation=(0,0,90),   # axle left-right
+                origin=(0,0,0),
+                scale=(1, math.copysign(1, x), 1)
+            )
+            Entity(parent=self,
+                model=copy(axle_mesh),
+                color=color.yellow,
+                position=(0, 0, z),
+                rotation=(0,0,90),   # axle left-right
+                origin=(0,0,0),
+                scale=(1, math.copysign(1, x), 1)
+           )
+            return wheel
 
-    return car
+        # Wheels
+        self.wheels = []
+        self.wheels.append(make_wheel(-wheel_position, 1.5))
+        self.wheels.append(make_wheel(wheel_position, 1.5))
+        self.wheels.append(make_wheel(-wheel_position, -1.5))
+        self.wheels.append(make_wheel( wheel_position, -1.5))
 
-car = make_car()
+    def update(self):
+        self.rotation_y += 60 * time.dt
+        for w in self.wheels:
+            w.rotation_x += 600 * time.dt
+            pass
+
+
+car = RaceCar()
 
 
 # Camera
 camera.position = (0, 3, -8)
-car.rotation_y += 45
 camera.look_at(car)
-
-def update():
-    car.rotation_y += 60 * time.dt
-    for w in car.wheels:
-        w.rotation_x += 600 * time.dt
-        pass
 
 app.run()
